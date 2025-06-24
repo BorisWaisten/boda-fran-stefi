a12346235290cd09edd3b962b6761d6ce20bd4d4
BorisWaisten <104634915+BorisWaisten@users.noreply.github.com>
2025-06-24 11:13:46 -0300

    data bank

--- components/Gifts/InfoGifts.jsx
+++ components/Gifts/InfoGifts.jsx
@@ -8,14 +8,14 @@ export default function InfoGifts() {
     return (
       <main className="min-h-screen m-0 overflow-x-hidden bg-primary">
         <div className="container text-center mx-auto py-10">
-          <div className=" bg-white p-6 rounded-lg shadow-lg">
+          <div className="bg-white p-6 rounded-lg shadow-lg mb-6">
             <h2 className="text-2xl font-bold mb-4">Datos Bancarios</h2>
             <ul className="list-none space-y-2">
-              <li><strong>Nombre del titular:</strong> Pepe Argento</li>
-              <li><strong>Cuil:</strong> 4545645645</li>
-              <li><strong>N° de cuenta:</strong> CA $ 4564564564</li>
-              <li><strong>Alias:</strong> pepeargento</li>
-              <li><strong>CBU:</strong> 4545645645645</li>
+              <li><strong>Nombre del titular:</strong> Diego Iván Merlo Gimenez</li>
+              <li><strong>Cuil:</strong> 20-39336845-5</li>
+              <li><strong>N° de cuenta:</strong> CA $ 36205341246852</li>
+              <li><strong>Alias:</strong> diegoivan1995</li>
+              <li><strong>CBU:</strong> 0110534630053412468529</li>
             </ul>
           </div>
           <div>
